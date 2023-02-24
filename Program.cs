0c59917afd2f9366e7dea43e0871b9e5de82de39
Sergey Dolzhenko <dsa1972@mail.ru>
2023-02-25 00:02:08 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿// Task 1.
+/*
+Console.Write("Input a first number: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input a second number: ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+int quad2 = number2 * number2;
+
+if (number1 == quad2)
+{
+  Console.WriteLine("Yes!");
+}
+else
+{
+  Console.WriteLine("No!");
+}
+*/
+
+// Task 2. Напишите программу, которая на вход принимает одно число (N),
+// а на выходе показывает все целые числа в прмежутке от -N до N.
+
+/*
+Console.Write("Input a positive number: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+int current = -num;
+
+while(current <= num)
+{
+  Console.Write(current + " ");
+  current += 2; // Если увеличить на 2 то можно писать current += 2; или current = current + 2;
+}
+*/
+
+
