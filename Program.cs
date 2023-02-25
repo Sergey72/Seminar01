e3c250af0ea2225144d90e6be3d1534cb29b1e7a
Sergey Dolzhenko <dsa1972@mail.ru>
2023-02-25 14:23:31 +0300

    add gitignore

--- Program.cs
+++ Program.cs
@@ -36,7 +36,7 @@ while(current <= num)
 // Задача 2: Напишите программу, которая на вход принимает два 
 // числа и выдаёт, какое число большее, а какое меньшее. 
 
-/*
+
 Console.Write("Input a first number: ");
 int number1 = Convert.ToInt32(Console.ReadLine());
 Console.Write("Input a second number: ");
@@ -50,7 +50,7 @@ else
 {
   Console.Write("max = " + number1);
 }
-*/
+
 
 // Задача 4: Напишите программу, которая принимает на вход три числа 
 // и выдаёт максимальное из этих чисел.
@@ -102,6 +102,7 @@ else
 // Задача 8: Напишите программу, которая на вход принимает число (N), 
 // а на выходе показывает все чётные числа от 1 до N.
 
+/*
 Console.Write("Введи любое целое число: ");
 int num = Convert.ToInt32(Console.ReadLine());
 int current = 2;
@@ -115,3 +116,4 @@ while (current <= num)
   }
 }
 
+*/
